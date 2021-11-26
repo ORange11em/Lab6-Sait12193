084bf072ae03de3f86004bb7c29f2f4af53e7442
sani <sanit_ak@cmu.ac.th>
2021-11-26 16:31:28 +0700

    แก้ชื่อแล้ว

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,31 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int N=100;
+    int i=0;
+    double x[N]={};
+    while ( x[N] == 0)
+    {
+        cout << "Enter an integer: "<<i+1;
+        cin >> x[i];
+        i++;
+    }
+    
+    int E=0;
+    int O=0;
+    int j=0;
+    while (j < i)
+    {
+        if ([j]%2 == 0){
+            E++;
+        }else{
+            O++;
+        } 
+    j++;   
+    }
+    cout << "#Even numbers = "<< E ;
+    cout << "#Odd numbers = " << O ;
+ 
     return 0;
+
 }
